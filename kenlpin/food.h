53be338ab130fc4b848ffea1a9c5a43945bb222d
kennypanjaitan <13521023@mahasiswa.itb.ac.id>
2022-10-27 23:10:55 +0700

    add ADT TIME

--- kenlpin/food.h
+++ kenlpin/food.h
@@ -1,6 +1,7 @@
 /* ADT Food */
 
 #include "../adete/boolean.h"
+#include "../adete/time.h"
 
 #ifndef FOOD_H
 #define FOOD_H
@@ -9,15 +10,16 @@ typedef struct
 {
     int id;
     char* name[50];
-    int exp;
-    int act;
-    int duration;
+    TIME exp;
+    char act;
+    TIME duration;
 } Food;
 
-#define ID(F) (F).id
-#define NAME(F) (F).name
-#define EXP(F) (F).exp
-#define ACT(F) (F).act
-#define DURATION(F) (F).duration
+#define ID(f) (f).id
+#define NAME(f) (f).name
+#define EXP(f) (f).exp
+#define ACT(f) (f).act
+#define DURATION(f) (f).duration
 
 #endif
+
