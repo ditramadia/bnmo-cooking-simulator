2f2867cc697cdb49a63275cc77e15530c97d4167
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2022-10-31 21:16:40 +0700

    edit welcome.txt

--- Inisiasi/splashScreen.c
+++ Inisiasi/splashScreen.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 #define MAX_LEN 128
 
-void print_image(FILE *fptr){
+void print_ascii(FILE *fptr){
   char read_string[MAX_LEN];
   while(fgets(read_string,sizeof(read_string),fptr) != NULL)
     printf("%s",read_string);
 }
-void splashScreenWelcome(){
+void Start(){
   char *filename;
   FILE *fptr;
   
   filename = "welcome.txt";
   fptr = fopen(filename,"r");
   system("cls");
-  print_image(fptr);
+  print_ascii(fptr);
   fclose(fptr);
   printf("\n");
   printf("");
 }
 
 int main(){
-  splashScreenWelcome();
+  Start();
 }
