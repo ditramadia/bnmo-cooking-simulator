3d5f1b8a9ecf865084086602b42a95e731f01ac9
Fahrian <73996601+fchrgrib2310@users.noreply.github.com>
2022-11-09 18:00:08 +0700

    tree revisi

--- src/app/notifikasi/notifikasi.c
+++ src/app/notifikasi/notifikasi.c
@@ -3,12 +3,16 @@
 
 
 void notifikasiExp(PrioQueueTime q){
-    if(Time(q)==0){
-        printf("%s sudah kedaluwarsa\n",Info(Head(q)));
+    if(!isEmpty(q)) {
+        if (Time(q) == 0) {
+            printf("%s sudah kedaluwarsa\n", Info(Head(q)));
+        }
     }
 }
 void notifikasiExp(PrioQueueTime q){
-    if(Time(q)==0){
-        printf("%s sudah masuk ke dalam inventory\n",Info(Head(q)));
+    if(!isEmpty(q)) {
+        if (Time(q) == 0) {
+            printf("%s sudah masuk ke dalam inventory\n", Info(Head(q)));
+        }
     }
 }
